35361aa42fba31c31e84a85430206ff5cc5980f6
ggmaleva <ggmaleva@yandex.ru>
2022-02-01 19:57:13 +0300

    init

---
+++ src/test/java/dev/vality/scrooge/ScroogeApplicationTest.java
@@ -0,0 +1,17 @@
+package dev.vality.scrooge;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ScroogeApplication.class)
+public class ScroogeApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
