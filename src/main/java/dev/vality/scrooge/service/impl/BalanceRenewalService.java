d0411c99b8974d4666d0033512da4f37689f422b
Gregory <32060161+ggmaleva@users.noreply.github.com>
2022-04-12 17:12:01 +0300

    Ft/shedlock (#22)
    * add shedlock
    
    * fix
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- src/main/java/dev/vality/scrooge/service/impl/BalanceRenewalService.java
+++ src/main/java/dev/vality/scrooge/service/impl/BalanceRenewalService.java
@@ -5,6 +5,7 @@ import dev.vality.scrooge.dao.domain.tables.pojos.Adapter;
 import dev.vality.scrooge.service.BalanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,7 @@ public class BalanceRenewalService {
     private final BalanceService<Adapter> adapterBalanceService;
 
     @Scheduled(cron = "${service.renewal.cron}")
+    @SchedulerLock(name = "balance_renewal_process")
     public void renew() {
         log.info("Start update balances");
         List<Adapter> adapters = adapterDao.getAll();
