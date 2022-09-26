2419afefe718283367c6dd96546527287fd3eb9e
malkoas <alien.life.form.1986@gmail.com>
2022-09-26 12:53:29 +0300

    fix configs

--- src/main/java/dev/vality/scrooge/config/KafkaConfig.java
+++ src/main/java/dev/vality/scrooge/config/KafkaConfig.java
@@ -17,8 +17,6 @@ import org.springframework.kafka.listener.DefaultErrorHandler;
 
 import java.util.Map;
 
-import static org.apache.kafka.clients.consumer.OffsetResetStrategy.LATEST;
-
 @EnableKafka
 @Configuration
 @RequiredArgsConstructor
@@ -38,7 +36,7 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, MachineEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         DefaultKafkaConsumerFactory<String, MachineEvent> consumerFactory =
-                new DefaultKafkaConsumerFactory<>(getConfigs());
+                new DefaultKafkaConsumerFactory<>(consumerConfigs());
         factory.setConsumerFactory(consumerFactory);
         factory.setConcurrency(withdrawalConcurrency);
         factory.setCommonErrorHandler(new DefaultErrorHandler());
@@ -47,14 +45,12 @@ public class KafkaConfig {
         return factory;
     }
 
-    private Map<String, Object> getConfigs() {
+    private Map<String, Object> consumerConfigs() {
         var properties = kafkaProperties.buildConsumerProperties();
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SinkEventDeserializer.class);
         properties.put(ConsumerConfig.CLIENT_ID_CONFIG, kafkaProperties.getClientId());
         properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, withdrawalMaxPollRecords);
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, LATEST.name().toLowerCase());
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         return properties;
     }
 
