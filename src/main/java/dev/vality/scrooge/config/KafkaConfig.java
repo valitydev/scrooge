15ba31699c01cf11196851d40283ee43398d8ac8
malkoas <alien.life.form.1986@gmail.com>
2022-09-26 11:54:04 +0300

    TD-394: add iam auth

--- src/main/java/dev/vality/scrooge/config/KafkaConfig.java
+++ src/main/java/dev/vality/scrooge/config/KafkaConfig.java
@@ -1,16 +1,12 @@
 package dev.vality.scrooge.config;
 
 import dev.vality.machinegun.eventsink.MachineEvent;
-import dev.vality.scrooge.config.properties.KafkaConsumerProperties;
-import dev.vality.scrooge.config.properties.KafkaSslProperties;
 import dev.vality.scrooge.listener.serde.SinkEventDeserializer;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -19,8 +15,6 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.kafka.clients.consumer.OffsetResetStrategy.LATEST;
@@ -30,15 +24,7 @@ import static org.apache.kafka.clients.consumer.OffsetResetStrategy.LATEST;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
-    private final KafkaSslProperties sslProperties;
-    private final KafkaConsumerProperties consumerProperties;
-
-
-    @Value("${kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${kafka.client-id}")
-    private String clientId;
+    private final KafkaProperties kafkaProperties;
 
     @Value("${kafka.topic.withdrawal.listener.max-poll-records}")
     private String withdrawalMaxPollRecords;
@@ -52,7 +38,7 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, MachineEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         DefaultKafkaConsumerFactory<String, MachineEvent> consumerFactory =
-                new DefaultKafkaConsumerFactory<>(consumerConfig(sslProperties));
+                new DefaultKafkaConsumerFactory<>(getConfigs());
         factory.setConsumerFactory(consumerFactory);
         factory.setConcurrency(withdrawalConcurrency);
         factory.setCommonErrorHandler(new DefaultErrorHandler());
@@ -61,35 +47,15 @@ public class KafkaConfig {
         return factory;
     }
 
-    private Map<String, Object> consumerConfig(KafkaSslProperties kafkaSslProperties) {
-        Map<String, Object> properties = new HashMap<>();
+    private Map<String, Object> getConfigs() {
+        var properties = kafkaProperties.buildConsumerProperties();
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SinkEventDeserializer.class);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerProperties.getGroupId());
-        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
+        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, kafkaProperties.getClientId());
         properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, withdrawalMaxPollRecords);
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, LATEST.name().toLowerCase());
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        properties.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, consumerProperties.getMaxPollIntervalMs());
-        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, consumerProperties.getMaxSessionTimeoutMs());
-        configureSsl(properties, kafkaSslProperties);
         return properties;
-    }
-
-    private void configureSsl(Map<String, Object> properties, KafkaSslProperties kafkaSslProperties) {
-        if (kafkaSslProperties.isEnabled()) {
-            properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name());
-            properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
-                    new File(kafkaSslProperties.getTrustStoreLocation()).getAbsolutePath());
-            properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, kafkaSslProperties.getTrustStorePassword());
-            properties.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, kafkaSslProperties.getKeyStoreType());
-            properties.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, kafkaSslProperties.getTrustStoreType());
-            properties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
-                    new File(kafkaSslProperties.getKeyStoreLocation()).getAbsolutePath());
-            properties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, kafkaSslProperties.getKeyStorePassword());
-            properties.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, kafkaSslProperties.getKeyPassword());
-        }
     }
 
 
