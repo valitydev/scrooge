2376711f23cc5f4252c3f520ecfbe1e90510f27d
f.shimich <fshim.dev@gmail.com>
2024-03-28 20:11:30 +0300

    IMP-123: add tests

--- src/main/java/dev/vality/scrooge/dao/TerminalBalanceDaoImpl.java
+++ src/main/java/dev/vality/scrooge/dao/TerminalBalanceDaoImpl.java
@@ -14,6 +14,10 @@ import static dev.vality.scrooge.dao.domain.Tables.*;
 @Component
 public class TerminalBalanceDaoImpl extends AbstractDao implements TerminalBalanceDao {
 
+    public final static String ACCOUNT_ID = "ACCOUNT_ID";
+    public final static String TERMINAL_ID = "TERMINAL_ID";
+    public final static String TERMINAL_NAME = "TERMINAL_NAME";
+
     private final TerminalBalanceMapper terminalBalanceMapper;
 
     public TerminalBalanceDaoImpl(DataSource dataSource, TerminalBalanceMapper terminalBalanceMapper) {
@@ -23,12 +27,14 @@ public class TerminalBalanceDaoImpl extends AbstractDao implements TerminalBalan
 
     @Override
     public List<TerminalBalance> getAllTerminalBalances() {
-        Query query = getDslContext().select(PROVIDER.ID, PROVIDER.NAME, ACCOUNT.ID, ACCOUNT.CURRENCY,
-                        TERMINAL.ID, TERMINAL.NAME, BALANCE.VALUE, BALANCE.TIMESTAMP)
-                .from(PROVIDER
+        Query query = getDslContext().select(PROVIDER.ID, PROVIDER.NAME, ACCOUNT.ID.as(ACCOUNT_ID), ACCOUNT.CURRENCY,
+                        ACCOUNT.NUMBER, TERMINAL.ID.as(TERMINAL_ID), TERMINAL.NAME.as(TERMINAL_NAME),
+                        BALANCE.VALUE, BALANCE.TIMESTAMP)
+                .from(TERMINAL
+                        .leftJoin(PROVIDER).on(PROVIDER.ID.eq(TERMINAL.PROVIDER_ID))
                         .leftJoin(ACCOUNT).on(PROVIDER.ID.eq(ACCOUNT.PROVIDER_ID))
-                        .leftJoin(TERMINAL).on(PROVIDER.ID.eq(TERMINAL.PROVIDER_ID))
-                        .leftJoin(BALANCE).on(ACCOUNT.ID.eq(BALANCE.ACCOUNT_ID)));
+                        .leftJoin(BALANCE).on(ACCOUNT.ID.eq(BALANCE.ACCOUNT_ID)))
+                .orderBy(PROVIDER.ID.desc());
         return fetch(query, terminalBalanceMapper);
     }
 
--- src/main/java/dev/vality/scrooge/dao/mapper/TerminalBalanceMapper.java
+++ src/main/java/dev/vality/scrooge/dao/mapper/TerminalBalanceMapper.java
@@ -1,7 +1,7 @@
 package dev.vality.scrooge.dao.mapper;
 
 import dev.vality.geck.common.util.TypeUtil;
-import dev.vality.scrooge.base.Balance;
+import dev.vality.scrooge.terminal.balance.Balance;
 import dev.vality.scrooge.terminal.balance.Provider;
 import dev.vality.scrooge.terminal.balance.Terminal;
 import dev.vality.scrooge.terminal.balance.TerminalBalance;
@@ -12,6 +12,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 
+import static dev.vality.scrooge.dao.TerminalBalanceDaoImpl.TERMINAL_ID;
+import static dev.vality.scrooge.dao.TerminalBalanceDaoImpl.TERMINAL_NAME;
 import static dev.vality.scrooge.dao.domain.Tables.*;
 
 @Component
@@ -19,18 +21,18 @@ public class TerminalBalanceMapper implements RowMapper<TerminalBalance> {
     @Override
     public TerminalBalance mapRow(ResultSet rs, int rowNum) throws SQLException {
         var terminal = new Terminal()
-                .setId(rs.getString(TERMINAL.ID.getName()))
-                .setName(rs.getString(TERMINAL.NAME.getName()));
+                .setId(rs.getString(TERMINAL_ID))
+                .setName(rs.getString(TERMINAL_NAME));
         var provider = new Provider()
                 .setId(rs.getString(PROVIDER.ID.getName()))
                 .setName(rs.getString(PROVIDER.NAME.getName()));
 
         var balance = new Balance()
-                .setAmount(rs.getLong(BALANCE.VALUE.getName()))
+                .setAmount(rs.getString(BALANCE.VALUE.getName()))
                 .setCurrencyCode(rs.getString(ACCOUNT.CURRENCY.getName()));
 
         return new TerminalBalance()
-                .setAccountId(rs.getString(ACCOUNT.ID.getName()))
+                .setAccountId(rs.getString(ACCOUNT.NUMBER.getName()))
                 .setLastUpdated(
                         TypeUtil.temporalToString(rs.getObject(BALANCE.TIMESTAMP.getName(), LocalDateTime.class)))
                 .setTerminal(terminal)
